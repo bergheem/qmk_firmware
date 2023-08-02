c376b6bcfe108b603e97502898fb35af9b7d1ff2
Federico Bergamo <bergamo.fed@gmail.com>
2023-08-02 11:16:25 +0200

    added bergheem keymap

---
+++ keyboards/sofle/keymaps/bergheem/rules.mk
@@ -0,0 +1,15 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# Build Options
+#   change yes to no to disable
+#
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+ENCODER_ENABLE = yes
+CONSOLE_ENABLE = no
+EXTRAKEY_ENABLE = yes
+UNICODEMAP_ENABLE = yes
